30790e9dd41231a73c2acc0e0e642c2a66a2af2e
Roy Chen <roychen4212@gmail.com>
2020-12-04 00:27:37 +0800

    disable warning logbox

--- app/App.js
+++ app/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
-import { Platform } from 'react-native';
+import { Platform, LogBox } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 import NavigationWrapper from '~/navigation';
+
+LogBox.ignoreLogs(['componentWillReceiveProps has been renamed']);
 
 const App = () => {
   useEffect(() => {
