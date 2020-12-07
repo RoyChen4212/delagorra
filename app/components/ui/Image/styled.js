ba85addea13e4a363c276623e27b10c372cbdc97
Roy Chen <roychen4212@gmail.com>
2020-12-08 01:18:09 +0800

    Remove unnecessary stylings

--- app/components/ui/Image/styled.js
+++ app/components/ui/Image/styled.js
@@ -1,16 +1,13 @@
 import FastImageUI from 'react-native-fast-image';
-
 import styled from 'styled-components/native';
-import { size, space, backgroundColor } from 'styled-system';
+import { size, space } from 'styled-system';
 
 export const Image = styled.Image`
   ${space}
   ${size}
-  ${backgroundColor}
 `;
 
 export const FastImage = styled(FastImageUI)`
   ${space}
   ${size}
-  ${backgroundColor}
 `;
