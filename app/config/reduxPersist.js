f5cb2cf20d833ed138c351bad37d4e6abf2fcc5a
Roy Chen <roychen4212@gmail.com>
2020-12-12 22:05:32 +0800

    Redux persist immutable implementation

--- app/config/reduxPersist.js
+++ app/config/reduxPersist.js
@@ -1,5 +1,7 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
+import { seamlessImmutableReconciler } from 'redux-persist-seamless-immutable';
+
 const REDUX_PERSIST = {
   active: true,
   reducerVersion: '1.0',
@@ -8,7 +10,7 @@ const REDUX_PERSIST = {
     key: 'primary',
     storage: AsyncStorage,
     whitelist: ['session'],
-    transforms: [],
+    stateReconciler: seamlessImmutableReconciler,
   },
 
   dataConfig: {
